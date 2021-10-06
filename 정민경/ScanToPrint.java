738d55b3991adfca7ec3de3d43e9ec1e86fbea53
정민경 <jmink002@gmail.com>
2021-10-06 21:45:25 +0900

    test_1

---
+++ 정민경/ScanToPrint.java
@@ -0,0 +1,19 @@
+package week2;
+
+import java.util.Scanner;
+
+public class ScanToPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String Hi = sc.nextLine();
+		
+		System.out.println(Hi);
+		
+		sc.close();
+
+	}
+
+}
