630873b792bd8156ccb6c4983bce981fdd3e13ab
purplepig4657 <purplepig4657@gmail.com>
2021-11-30 12:17:44 +0900

    2021. 11. 30. [2743]단어 길이 재기

---
+++ 김태현/[2021.11.30]2743.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CountingWordLength {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine().length());
+
+    }
+}
