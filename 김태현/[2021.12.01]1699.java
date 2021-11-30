06adc4b1c597c0453f73f5bd9f53e0664f081bb4
purplepig4657 <purplepig4657@gmail.com>
2021-12-01 01:40:17 +0900

    2021. 12. 01. [1699]제곱수의 합

---
+++ 김태현/[2021.12.01]1699.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SquareNumberSum {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+
+        for(int i = 1; i <= N; i++) {
+            dp[i] = i;
+        }
+
+        for(int i = 2; i <= N; i++) {
+            for(int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
